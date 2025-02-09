7d1992dceb75b3e3d9bb86e2078d3d88f4b915aa
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2025-02-09 15:55:13 +0200

    Додав консольний проєкт C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Обчислення суми двох чисел");
+            Console.WriteLine("Перше число:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Друге число:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("Сума чисел: " + sum);
+        }
+    }
+}
